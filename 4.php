913ee21b54b4f9a16c5195851f7eb8b279e9b9e4
nazarevrn <kimi-champ@mail.ru>
2020-12-13 16:27:26 +0300

    add tack 4 st2

--- 4.php
+++ 4.php
@@ -3,8 +3,9 @@
 //добавим индекс для color
 // CREATE INDEX color ON products(color)
 
-/*сформируем массив, в котором перечислены цвета и насколько нужно
-изменить цену товаров указанного цвета
+/*
+ * сформируем массив, в котором перечислены цвета и насколько нужно
+ * изменить цену товаров указанного цвета
 */
 
 $data = [
@@ -12,6 +13,7 @@ $data = [
     'green' => '1.1',
 ];
 
+//вариант 1. количество запросов = количеству цветов
 foreach ($data as $color => $value) {
     //псевдозапуск команды используя query builder yii
     \Yii::$app->db->createCommand("UPDATE products SET price = price * :price_value WHERE color=':color'")
@@ -19,3 +21,20 @@ foreach ($data as $color => $value) {
             ->bindValue(':color', $color)
             ->execute();
 }
+
+//вариант 2. сгенерируем 1 запрос для всех цветов
+
+$query = 'UPDATE
+            products
+        SET
+            price = price *
+            CASE';
+
+foreach ($data as $color => $value) {
+    $query .= "WHEN `color` = '$color' THEN $value ";
+}
+
+//ELSE 1 - для того, что бюы не изменить цену товаров, цвет которых не перечислен в params
+$query .= 'ELSE 1
+	END';
+\Yii::$app->db->createCommand($query)->execute();
